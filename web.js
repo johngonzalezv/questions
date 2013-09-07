303838667ed42cca885ac0d57b00a86ff0e12e80
John Gonzalez <johngonzalezv@gmail.com>
2013-09-07 18:15:36 +0000

    new page questions

--- web.js
+++ web.js
@@ -12,9 +12,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
 
+//Para cargar archivos de otras carpetas
+app.use(express.static(__dirname));
+
+
 // Render homepage (note trailing slash): example.com/
 app.get('/', function(request, response) {
-  var data = fs.readFileSync('index.html').toString();
+  var data = fs.readFileSync('carousel.html').toString();
   response.send(data);
 });
 
